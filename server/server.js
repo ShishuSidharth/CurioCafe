f3fefbe53e0aacc59ddd647b90658994bc288d21
Sidharth Sharma <shishu.jsrsidh@gmail.com>
2024-09-17 15:40:36 +0530

    asd

--- server/server.js
+++ server/server.js
@@ -13,7 +13,17 @@ app.use(express.json());
 app.use("/api/users", usersRoute);
 app.use("/api/questions", questionsRoute);
 app.use("/api/question-actions", questionActionsRoute);
-
+const __dirname1=path.resolve();
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"client", "build", "index.html"));
+  })
+}else{
+  app.get('/',(req,res)=>{
+    res.send('API ia running successfully')
+  });
+}
 const port = process.env.PORT || 5000;
 const server = require("http").createServer(app);
 
