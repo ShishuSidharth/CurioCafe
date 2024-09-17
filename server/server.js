19cedb5258c0e83b6f338eb1924d12a7bfece86d
Sidharth Sharma <shishu.jsrsidh@gmail.com>
2024-09-17 16:45:34 +0530

    a

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ io.on("connection", (socket) => {
 __dirname = path.resolve();
 // render deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
